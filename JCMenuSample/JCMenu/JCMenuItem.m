326b96b1fd97662e4d08575d672f1c5904c27d0c
jbaptistecastro <jeanbaptiste.castro@gmail.com>
2013-11-07 16:50:19 +0100

    Cop

--- JCMenuSample/JCMenu/JCMenuItem.m
+++ JCMenuSample/JCMenu/JCMenuItem.m
@@ -5,7 +5,7 @@
 
 /*
  *
- * Copyright (c) 2013 Jean-Baptiste Castro
+ * Copyright (c) 2013 Jean-Baptiste Castro (@jbaptistecastro)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
